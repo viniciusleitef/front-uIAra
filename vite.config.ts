e651f778662757f07f7bf084faefc98c90a73e78
Vinícius Leite <viniciusleitealves@gmail.com>
2024-09-27 12:24:30 -0300

    add new files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import { loadEnv, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+process.env = { ...process.env, ...loadEnv("production", process.cwd()) };
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
 })
