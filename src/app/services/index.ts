3fb014a9f0ea2f2fae6b8ca43d256dbda0eaaf97
viniciusleitealves <viniciusleitealves@gmail.com>
2024-11-05 16:32:12 -0300

    admin page

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: "https://api.detectai.com.br/",
+  baseURL: "http://127.0.0.1:8000/",
 });
 
 api.interceptors.request.use(
